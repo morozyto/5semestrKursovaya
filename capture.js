e3bc00d7a5f487255d4527f239310ac1219e05a2
Антон <moranton@mail.ru>
2019-01-05 19:41:42 +0300

    save state with Kanade

--- capture.js
+++ capture.js
@@ -3,23 +3,110 @@
 function min(a, b) {
     if (a > b)
         return b;
-    else
-        return a;
+    return a;
 }
 
 function max(a, b) {
     if (a > b)
         return a;
-    else
-        return b;
+    return b;
 }
+
+function abs(a) {
+    if (a > 0)
+        return a;
+    return -a;
+}
+
+function getGauss(new_x, new_y, otklonenie) {
+    return (1. / (Math.sqrt(2*Math.PI) * otklonenie)) * Math.exp(- (new_x*new_x + new_y*new_y) / 2*otklonenie*otklonenie);
+}
+
+function getXDerivative(dx, dy, old_image, new_image, i, j) {
+    return (1. / 4.) * dx * ((getI(old_image, i + dx, j) + getI(old_image, i + dx, j + dy) + getI(new_image, i + dx, j) + getI(new_image, i + dx, j + dy))
+        - (getI(old_image, i, j) + getI(old_image, i, j + dy) + getI(new_image, i, j) + getI(new_image, i, j + dy)));
+}
+
+function getYDerivative(dx, dy, old_image, new_image, i, j) {
+    return (1. / 4.) * dy * ((getI(old_image, i, j + dy) + getI(old_image, i + dx, j + dy) + getI(new_image, i, j + dy) + getI(new_image, i + dx, j + dy))
+        - (getI(old_image, i, j) + getI(old_image, i + dx, j) + getI(new_image, i, j) + getI(new_image, i + dx, j)));
+}
+
+function getTDerivative(dx, dy, old_image, new_image, i, j) {
+    return (1. / 4.) * ((getI(new_image, i, j) + getI(new_image, i + dx, j) + getI(new_image, i, j + dy) + getI(new_image, i + dx, j + dy))
+        - (getI(old_image, i, j) + getI(old_image, i + dx, j) + getI(old_image, i, j + dy) + getI(old_image, i + dx, j + dy)));
+}
+
+function getNewCoords(x_centre, y_centre, x, y) {
+    return {x: x - x_centre, y: y - y_centre};
+}
+
+function getOldCoords(x_centre, y_centre, x_new, y_new) {
+    return {x: x_new + x_centre, y: y_new + y_centre};
+}
+
+function getI(imageData, x, y) {
+
+    var data = imageData.data;
+
+    let index = ((y * (imageData.width * 4)) + (x * 4));
+
+    let R = data[index + 0];
+    let G = data[index + 1];
+    let B = data[index + 2];
+
+    return (0.2126*R + 0.7152*G + 0.0722*B);
+}
+
+function found_move(context_old, context_new, x_start, y_start, neighborhood, maxWidth, maxHeight) {
+
+    if (x_start < neighborhood || y_start < neighborhood || (maxWidth - x_start) <= neighborhood || (maxHeight - y_start) <= neighborhood)
+        return {x: 0, y: 0};
+
+    var oldImageData = context_old.getImageData(x_start - neighborhood, y_start - neighborhood, 2*neighborhood + 1, 2*neighborhood + 1);
+    var newImageData = context_new.getImageData(x_start - neighborhood, y_start - neighborhood, 2*neighborhood + 1, 2*neighborhood + 1);
+
+    let a = 0, b = 0, c = 0, d = 0, d1 = 0, d2 = 0;
+
+    for (let i = -neighborhood; i <= neighborhood; i++) {
+        for (let j = -neighborhood; j <= neighborhood; j++) {
+            let cur_i = x_start + i;
+            let cur_j = y_start + j;
+            let newCoords = getNewCoords(x_start, y_start, cur_i, cur_j);
+            let weight = getGauss(newCoords.x, newCoords.y, 2);
+
+            let x_deriv = getXDerivative(1, 1, oldImageData, newImageData, cur_i, cur_j);
+            let y_deriv = getYDerivative(1, 1, oldImageData, newImageData, cur_i, cur_j);
+            let t_deriv = getTDerivative(1, 1, oldImageData, newImageData, cur_i, cur_j);
+
+            a = a + weight * x_deriv * x_deriv;
+            b = b + weight * x_deriv * y_deriv;
+            d = d + weight * y_deriv * y_deriv;
+
+            d1 = d1 - weight * t_deriv * x_deriv;
+            d2 = d2 - weight * t_deriv * y_deriv;
+        }
+    }
+    c = b;
+
+    let det = a*d - b*c;
+    if (det === 0) {
+        alert("det is 0");
+        return {x: 0, y: 0};
+    }
+
+    return {x: (d*d1 - b*d2) / det, y: (-c*d1 + a*d2) / det};   // Vector of movement
+}
+
+var previous_image;
 
 window.onload = function () {
 
     let photo = document.getElementById('photo');
     let canvas = document.getElementById('canvas');
     let video = document.getElementById('video');
-    let button = document.getElementById('button');
+    let capture_button = document.getElementById('capture_button');
+    let previous_button = document.getElementById('previous_image_button');
     let allow = document.getElementById('allow');
     let context = canvas.getContext('2d');
     let videoStreamUrl = false;
@@ -28,8 +115,8 @@ window.onload = function () {
     var captureMe = function () {
         if (!videoStreamUrl) alert('Allow camera first!');
 
+        previous_image = photo.src;
         photo.src = start(video,
-
 
             function(context, leftX, rightX, downY, upY) {
 
@@ -39,7 +126,7 @@ window.onload = function () {
                 var sum = 0;
 
                 for (var i = 0; i < data.length; i += 4) {
-                    if (data[i] > 3*max(data[i + 1], data[i + 2]) / 2)
+                    if (data[i] > 12*max(data[i + 1], data[i + 2]) / 7)
                         sum += 1;
                 }
 
@@ -80,22 +167,19 @@ window.onload = function () {
             });
     };
 
-    button.addEventListener('click', captureMe);
+    capture_button.addEventListener('click', captureMe);
 
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    window.URL.createObjectURL = window.URL.createObjectURL || window.URL.webkitCreateObjectURL || window.URL.mozCreateObjectURL || window.URL.msCreateObjectURL;
+    //window.URL.createObjectURL = window.URL.createObjectURL || window.URL.webkitCreateObjectURL || window.URL.mozCreateObjectURL || window.URL.msCreateObjectURL;
 
     navigator.getUserMedia({video: true}, function (stream) {
         allow.style.display = "none";
 
-        videoStreamUrl = window.URL.createObjectURL(stream);
-
-        video.src = videoStreamUrl;
+        video.srcObject = stream;
     }, function () {
         alert('Oh, i am sorry, did i break your concentration? TURN THE CAMERA ON!');
     });
 };
-
 
 function start(video, find_callback, mark_callback) {
 
@@ -108,7 +192,7 @@ function start(video, find_callback, mark_callback) {
     let max = min(maxHeigth, maxWidth);
 
 
-    for (var currentSize = 150; currentSize < max; currentSize = currentSize + 50) {
+    for (var currentSize = 30; currentSize < max; currentSize = currentSize + 40) {
 
         let step = currentSize / 4;
 
