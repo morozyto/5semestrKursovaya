86369f664097123432d03e42e6537f1e215cb7a0
Антон <moranton@mail.ru>
2019-01-07 03:57:44 +0300

    kanade 90% work!

--- capture.js
+++ capture.js
@@ -18,24 +18,6 @@ function abs(a) {
     return -a;
 }
 
-function getHeatMap(canvas, imagedata) {
-
-    let data = imagedata.data;
-
-    for (var i = 0; i < data.length; i += 4) {
-        let R = data[i + 0];
-        let G = data[i + 1];
-        let B = data[i + 2];
-        let I = 0.2126*R + 0.7152*G + 0.0722*B;
-        data[i + 0] = I;
-        data[i + 1] = data[i + 2] = 0;
-    }
-
-    canvas.getContext('2d').putImageData(imagedata, 0, 0);
-
-    return canvas.toDataURL('image/png');
-}
-
 function draw_strela(ctx, x_start, y_start, dx, dy) {
     ctx.strokeStyle = "black";
     ctx.beginPath();
@@ -69,19 +51,6 @@ function getTDerivative(dx, dy, get_i_old, get_i, i, j) {
         - (get_i_old(i, j) + get_i_old(i + dx, j) + get_i_old(i, j + dy) + get_i_old(i + dx, j + dy)));
 }
 
-
-
-
-
-
-function getNewCoords(x_centre, y_centre, x, y) {
-    return {x: x - x_centre, y: y - y_centre};
-}
-
-function getOldCoords(x_centre, y_centre, x_new, y_new) {
-    return {x: x_new + x_centre, y: y_new + y_centre};
-}
-
 function found_move(get_deriv, x_start, y_start, neighborhood, maxWidth, maxHeight, verbose, get_weight) {
 
     if (x_start < neighborhood || y_start < neighborhood || (maxWidth - x_start) <= neighborhood || (maxHeight - y_start) <= neighborhood)
@@ -93,7 +62,6 @@ function found_move(get_deriv, x_start, y_start, neighborhood, maxWidth, maxHeig
         for (let j = -neighborhood; j <= neighborhood - 1; j++) {
             let cur_i = x_start + i;
             let cur_j = y_start + j;
-            //let newCoords = getNewCoords(x_start, y_start, cur_i, cur_j);
             let weight = get_weight(i, j);
 
             let x_deriv = get_deriv(cur_i, cur_j, 0);
@@ -116,14 +84,9 @@ function found_move(get_deriv, x_start, y_start, neighborhood, maxWidth, maxHeig
     }
     c = b;
 
-    // alert("a is " + a);
-
     let det = a*d - b*c;
     if (det === 0) {
-       // alert("det is 0");
         return {x: 0, y: 0};
-    } else {
-        //  alert("det is " + det);
     }
 
     let answer = {x: (d*d1 - b*d2) / det, y: (-c*d1 + a*d2) / det};
@@ -135,85 +98,24 @@ function found_move(get_deriv, x_start, y_start, neighborhood, maxWidth, maxHeig
     return answer;   // Vector of movement
 }
 
-/*
-function found_move(oldImageData, newImageData, x_start, y_start, neighborhood, maxWidth, maxHeight, verbose) {
-
-    if (x_start < neighborhood || y_start < neighborhood || (maxWidth - x_start) <= neighborhood || (maxHeight - y_start) <= neighborhood)
-        return {x: 0, y: 0};
-
-   // var oldImageData = context_old.getImageData(x_start - neighborhood, y_start - neighborhood, 2*neighborhood + 1, 2*neighborhood + 1);
-    //var newImageData = context_new.getImageData(x_start - neighborhood, y_start - neighborhood, 2*neighborhood + 1, 2*neighborhood + 1);
-
-    let a = 0, b = 0, c = 0, d = 0, d1 = 0, d2 = 0;
-
-   // alert("first a is " + a);
-
-    for (let i = -neighborhood + 1; i <= neighborhood - 1; i++) {
-        for (let j = -neighborhood + 1; j <= neighborhood - 1; j++) {
-            let cur_i = x_start + i;
-            let cur_j = y_start + j;
-            let newCoords = getNewCoords(x_start, y_start, cur_i, cur_j);
-            let weight = getGauss(newCoords.x, newCoords.y, 0.2);
-
-            let x_deriv = getXDerivative(1, 1, oldImageData, newImageData, newCoords.x, newCoords.y);
-            let y_deriv = getYDerivative(1, 1, oldImageData, newImageData, newCoords.x, newCoords.y);
-            let t_deriv = getTDerivative(1, 1, oldImageData, newImageData, newCoords.x, newCoords.y);
-
-            if (verbose)
-                alert('x_deriv is ' + x_deriv + ' y_deriv is ' + y_deriv + ' t_deriv is ' + t_deriv + ' i = ' + i + ' j = ' + j);
-
-
-            a = a + weight * x_deriv * x_deriv;
-            b = b + weight * x_deriv * y_deriv;
-            d = d + weight * y_deriv * y_deriv;
-
-            d1 = d1 - weight * t_deriv * x_deriv;
-            d2 = d2 - weight * t_deriv * y_deriv;
-        }
-    }
-    c = b;
-
-   // alert("a is " + a);
-
-    let det = a*d - b*c;
-    if (det === 0) {
-        alert("det is 0");
-        return {x: 0, y: 0};
-    } else {
-      //  alert("det is " + det);
-    }
-
-    let answer = {x: (d*d1 - b*d2) / det, y: (-c*d1 + a*d2) / det};
-
-    if (verbose)
-        alert('answer.x = ' + answer.x + ' , answer.y = ' + answer.y);
-
-
-    return answer;   // Vector of movement
-}
-*/
 window.onload = function () {
 
     let photo = document.getElementById('photo');
     let photoOld = document.getElementById('photoprev');
 
-    let photoint = document.getElementById('photo');
-    let photoOldint = document.getElementById('photoprev');
 
     let canvas = document.getElementById('canvas');
     let canvas1 = document.getElementById('canvas1');
 
-    let canvas2 = document.getElementById('canvas2');
-    let canvas3 = document.getElementById('canvas3');
 
     let video = document.getElementById('video');
     let capture_button = document.getElementById('capture_button');
-   // let previous_button = document.getElementById('previous_image_button');
     let allow = document.getElementById('allow');
     let context = canvas.getContext('2d');
     let context1 = null; //canvas1.getContext('2d');
 
     // функция которая будет выполнена при нажатии на кнопку захвата кадра
+
     var captureMe = function () {
 
         if (context1) {
@@ -222,6 +124,7 @@ window.onload = function () {
             photo.src = canvas.toDataURL('image/png');
 
             let future_previous = context.getImageData(0, 0, video.width, video.height);
+         //   photoint.src = getHeatMap(canvas2, future_previous);
             let is = [];
             let derivs = [];
 
@@ -275,7 +178,7 @@ window.onload = function () {
                 }
             }
 
-            let neighborhood = 50;
+            let neighborhood = 10;
 
 
             for (let i = 0; i < video.width - 1; i++) {
@@ -302,7 +205,7 @@ window.onload = function () {
                     weights[j*neighborhood + i] = getGauss(i, j, 0.2);
 
                 }
-            };
+            }
 
             let get_weight = function(x, y) {
                 return weights[y*neighborhood + x];
@@ -320,30 +223,7 @@ window.onload = function () {
                 }
             }
 
-
-/*
-
-
-
-            for (let i = 0; i < video.width; i++) {
-                if (i % 50 === 0)
-                    console.log("current width", i);
-                for (let j = 0; j < video.height; j++) {
-                    let verbose = false;//i % 10 === 0 & j % 10 === 0;
-                    let diff = found_move(context.getImageData(i - neighborhood, j - neighborhood, 2*neighborhood + 1, 2*neighborhood + 1),
-                        context1.getImageData(i - neighborhood, j - neighborhood, 2*neighborhood + 1, 2*neighborhood + 1),
-                        i, j, neighborhood, video.width, video.height, verbose);
-
-                  //  let diff = found_move(get_i, g)
-
-
-                    if (i % 5 === 0 & j % 5 === 0)
-                        draw_strela(context, i, j, diff.x, diff.y);
-                }
-            }
-*/
-            photoint.src = getHeatMap(canvas2, future_previous);
-            photoOldint.src = getHeatMap(canvas3, context1.getImageData(0, 0, video.width, video.height));
+          //  photoOldint.src = getHeatMap(canvas3, context1.getImageData(0, 0, video.width, video.height));
 
             photo.src = canvas.toDataURL('image/png');
             photoOld.src = canvas1.toDataURL('image/png');
@@ -358,8 +238,6 @@ window.onload = function () {
 
     };
 
-    capture_button.addEventListener('click', captureMe);
-
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
     navigator.getUserMedia({video: true}, function (stream) {
@@ -369,4 +247,9 @@ window.onload = function () {
     }, function () {
         alert('Oh, i am sorry, did i break your concentration? TURN THE CAMERA ON!');
     });
+
+     setTimeout(function run() {
+         captureMe();
+         setTimeout(run, 1);
+     }, 1000);
 };
